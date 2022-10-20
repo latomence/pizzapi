281d3f42b9173552ac0206b763c3f7807b1439e5
Tom <tom.perret51@gmail.com>
2022-10-20 14:44:01 +0200

    start tp 1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PizzasModule } from './pizzas/pizzas.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    PizzasModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
